3d3de9b51a255aed10e54d0f1fb0b4ba4a60e673
votex12 <42341500+votex12@users.noreply.github.com>
2019-03-16 14:06:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,26 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'test') {
-    	message.reply('Test');
-  	}
+client.on('message', msg => {
+  if (msg.content === '!kgmalden') {
+    msg.reply('Grade KG auf Malden [Joinen]');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
+client.on('message', msg => {
+  if (msg.content === '!kgtanoa') {
+    msg.reply('Grade KG auf Malden [Joinen]');
+  }96
+});
+
+client.on('message', msg => {
+  if (msg.content === '!kgaltis') {
+    msg.reply('Grade KG auf Altis [Joinen]');
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
